f533e05641a1ce6e215f01215096ddc389fc51f1
Anders Flemmen <anders.flemmen@dfind.no>
2023-12-05 13:35:20 +0100

    Oppgradering av Jetty til versjon 12 (#256)
    * Oppgradering av Jetty til versjon 12
    
    * Setter content-type

--- svarut-rest-klient/src/main/java/no/ks/fiks/svarut/klient/SvarUtKlientApiImpl.java
+++ svarut-rest-klient/src/main/java/no/ks/fiks/svarut/klient/SvarUtKlientApiImpl.java
@@ -1,31 +1,30 @@
 package no.ks.fiks.svarut.klient;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import no.ks.fiks.svarut.klient.exceptions.HttpException;
 import no.ks.fiks.svarut.klient.model.Error;
 import no.ks.fiks.svarut.klient.model.*;
-import org.eclipse.jetty.client.HttpClient;
-import org.eclipse.jetty.client.api.Authentication;
-import org.eclipse.jetty.client.api.ContentResponse;
-import org.eclipse.jetty.client.api.Request;
-import org.eclipse.jetty.client.dynamic.HttpClientTransportDynamic;
-import org.eclipse.jetty.client.util.BasicAuthentication;
-import org.eclipse.jetty.client.util.InputStreamContentProvider;
-import org.eclipse.jetty.client.util.MultiPartContentProvider;
-import org.eclipse.jetty.client.util.StringContentProvider;
+import org.eclipse.jetty.client.*;
+import org.eclipse.jetty.client.transport.HttpClientTransportDynamic;
 import org.eclipse.jetty.http.HttpMethod;
+import org.eclipse.jetty.http.MultiPart;
 import org.eclipse.jetty.io.ClientConnector;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 
 import java.io.InputStream;
 import java.net.URI;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class SvarUtKlientApiImpl implements SvarUtKlientApi {
+
+    private static final String FORSENDELSE_PART = "forsendelse";
+    private static final String FILER_PART = "filer";
+
     private final ObjectMapper objectMapper = new ObjectMapper();
     private final String baseUrl;
 
@@ -79,23 +78,16 @@ public class SvarUtKlientApiImpl implements SvarUtKlientApi {
     @Override
     @Deprecated
     public ForsendelsesId sendForsendelseMedId(Forsendelse forsendelse, ForsendelsesId forsendelsesId, Map<String, InputStream> data) {
-        if(forsendelse == null) throw new IllegalArgumentException("Forsendelse kan ikke være null");
+        if (forsendelse == null) throw new IllegalArgumentException("Forsendelse kan ikke være null");
         validateForNull(forsendelsesId);
-        if(data == null) throw new IllegalArgumentException("Data kan ikke være null");
+        if (data == null) throw new IllegalArgumentException("Data kan ikke være null");
         try {
-            MultiPartContentProvider multipart = new MultiPartContentProvider();
-            multipart.addFieldPart("forsendelse", new StringContentProvider(objectMapper.writeValueAsString(forsendelse)), null);
-            for (Dokument dokument : forsendelse.getDokumenter()) {
-                multipart.addFilePart("filer", dokument.getFilnavn(), new InputStreamContentProvider(data.get(dokument.getFilnavn())), null);
-            }
-
-            multipart.close();
-
-            final Request request = client.newRequest(baseUrl + "/tjenester/api/forsendelse/v1/" + forsendelsesId.getId().toString() + "/sendForsendelse");
+            final Request request = client
+                    .newRequest(baseUrl + "/tjenester/api/forsendelse/v1/" + forsendelsesId.getId().toString() + "/sendForsendelse")
+                    .method(HttpMethod.POST)
+                    .body(buildMultipartContent(forsendelse, data))
+                    .idleTimeout(16, TimeUnit.MINUTES);
             addAuth(request);
-            request.content(multipart);
-            request.method(HttpMethod.POST);
-            request.idleTimeout(16, TimeUnit.MINUTES);
             final ContentResponse send = request.send();
 
             if (send.getStatus() != 200) {
@@ -112,14 +104,12 @@ public class SvarUtKlientApiImpl implements SvarUtKlientApi {
 
     @Override
     public void slettForsendelseDokumenter(ForsendelsesId... forsendelseId) {
-        if(forsendelseId == null || forsendelseId.length == 0 || Arrays.stream(forsendelseId).anyMatch(Objects::isNull)) throw new IllegalArgumentException("ForsendelseId kan ikke være null");
-        final Request request = client.newRequest(baseUrl + "/tjenester/api/forsendelse/v1/slettFiler");
-        request.method(HttpMethod.POST);
+        if (forsendelseId == null || forsendelseId.length == 0 || Arrays.stream(forsendelseId).anyMatch(Objects::isNull)) throw new IllegalArgumentException("ForsendelseId kan ikke være null");
+        final Request request = client.newRequest(baseUrl + "/tjenester/api/forsendelse/v1/slettFiler")
+                .method(HttpMethod.POST)
+                .body(buildJsonStringContent(forsendelseId));
         addAuth(request);
         try {
-            final String content = objectMapper.writeValueAsString(forsendelseId);
-
-            request.content(new StringContentProvider("application/json", content, Charset.forName("UTF-8")));
             final ContentResponse send = request.send();
             if (send.getStatus() != 204) {
                 throw getHttpException(send);
@@ -133,22 +123,14 @@ public class SvarUtKlientApiImpl implements SvarUtKlientApi {
 
     @Override
     public ForsendelsesId sendForsendelse(Forsendelse forsendelse, Map<String, InputStream> data) {
-        if(forsendelse == null) throw new IllegalArgumentException("Forsendelse kan ikke være null");
-        if(data == null) throw new IllegalArgumentException("Data kan ikke være null");
+        if (forsendelse == null) throw new IllegalArgumentException("Forsendelse kan ikke være null");
+        if (data == null) throw new IllegalArgumentException("Data kan ikke være null");
         try {
-            MultiPartContentProvider multipart = new MultiPartContentProvider();
-            multipart.addFieldPart("forsendelse", new StringContentProvider(objectMapper.writeValueAsString(forsendelse)), null);
-            for (Dokument dokument : forsendelse.getDokumenter()) {
-                multipart.addFilePart("filer", dokument.getFilnavn(), new InputStreamContentProvider(data.get(dokument.getFilnavn())), null);
-            }
-
-            multipart.close();
-
-            final Request request = client.newRequest(baseUrl + "/tjenester/api/forsendelse/v1/sendForsendelse");
+            final Request request = client.newRequest(baseUrl + "/tjenester/api/forsendelse/v1/sendForsendelse")
+                    .method(HttpMethod.POST)
+                    .body(buildMultipartContent(forsendelse, data))
+                    .idleTimeout(16, TimeUnit.MINUTES);
             addAuth(request);
-            request.content(multipart);
-            request.method(HttpMethod.POST);
-            request.idleTimeout(16, TimeUnit.MINUTES);
             final ContentResponse send = request.send();
 
             if (send.getStatus() != 200) {
@@ -169,6 +151,7 @@ public class SvarUtKlientApiImpl implements SvarUtKlientApi {
 
         return hentStatus(forsendelseId.getId());
     }
+
     @Override
     public ForsendelseStatus hentStatus(UUID forsendelseId) {
         validateForNull(forsendelseId);
@@ -192,20 +175,18 @@ public class SvarUtKlientApiImpl implements SvarUtKlientApi {
 
     @Override
     public List<ForsendelseStatus> hentStatuser(ForsendelsesId... forsendelseId) {
-        if(forsendelseId == null || forsendelseId.length == 0 || Arrays.stream(forsendelseId).anyMatch((v) -> v == null || v.getId() == null)) throw new IllegalArgumentException("ForsendelseId kan ikke være null");
-        return hentStatuser(Arrays.stream(forsendelseId).map(v -> v.getId()).toArray(UUID[]::new));
+        if (forsendelseId == null || forsendelseId.length == 0 || Arrays.stream(forsendelseId).anyMatch((v) -> v == null || v.getId() == null)) throw new IllegalArgumentException("ForsendelseId kan ikke være null");
+        return hentStatuser(Arrays.stream(forsendelseId).map(ForsendelsesId::getId).toArray(UUID[]::new));
     }
 
     @Override
     public List<ForsendelseStatus> hentStatuser(UUID... forsendelseId) {
-        if(forsendelseId == null || forsendelseId.length == 0 || Arrays.stream(forsendelseId).anyMatch((v) -> v == null)) throw new IllegalArgumentException("ForsendelseId kan ikke være null");
-        final Request request = client.newRequest(baseUrl + "/tjenester/api/forsendelse/v1/statuser");
-        request.method(HttpMethod.POST);
+        if (forsendelseId == null || forsendelseId.length == 0 || Arrays.stream(forsendelseId).anyMatch(Objects::isNull)) throw new IllegalArgumentException("ForsendelseId kan ikke være null");
+        final Request request = client.newRequest(baseUrl + "/tjenester/api/forsendelse/v1/statuser")
+                .method(HttpMethod.POST)
+                .body(buildJsonStringContent(forsendelseId));
         addAuth(request);
         try {
-            final String content = objectMapper.writeValueAsString(forsendelseId);
-
-            request.content(new StringContentProvider("application/json", content, Charset.forName("UTF-8")));
             final ContentResponse send = request.send();
             if (send.getStatus() == 404) return null;
             if (send.getStatus() == 200)
@@ -254,13 +235,13 @@ public class SvarUtKlientApiImpl implements SvarUtKlientApi {
     }
 
     @Override
-    public List<DokumentMetadata> retrieveDokumentMetadata(ForsendelsesId forsendelseid){
+    public List<DokumentMetadata> retrieveDokumentMetadata(ForsendelsesId forsendelseid) {
         validateForNull(forsendelseid);
         return retrieveDokumentMetadata(forsendelseid.getId());
     }
 
     @Override
-    public List<DokumentMetadata> retrieveDokumentMetadata(UUID forsendelseid){
+    public List<DokumentMetadata> retrieveDokumentMetadata(UUID forsendelseid) {
         validateForNull(forsendelseid);
         final Request request = client.newRequest(baseUrl + "/tjenester/api/forsendelse/v1/" + forsendelseid + "/dokumentMetadata");
         addAuth(request);
@@ -272,7 +253,7 @@ public class SvarUtKlientApiImpl implements SvarUtKlientApi {
             else {
                 throw getHttpException(send);
             }
-        }catch (RuntimeException e) {
+        } catch (RuntimeException e) {
             throw e;
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -280,7 +261,7 @@ public class SvarUtKlientApiImpl implements SvarUtKlientApi {
     }
 
     @Override
-    public List<String> forsendelseTyper(){
+    public List<String> forsendelseTyper() {
         final Request request = client.newRequest(baseUrl + "/tjenester/api/forsendelse/v1/forsendelseTyper");
         addAuth(request);
         try {
@@ -291,7 +272,7 @@ public class SvarUtKlientApiImpl implements SvarUtKlientApi {
             else {
                 throw getHttpException(send);
             }
-        }catch (RuntimeException e) {
+        } catch (RuntimeException e) {
             throw e;
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -300,8 +281,8 @@ public class SvarUtKlientApiImpl implements SvarUtKlientApi {
 
 
     @Override
-    public List<MottakerForsendelsesTyper> retrieveMottakersystemForOrgnr(String orgnr){
-        if(orgnr == null || orgnr.length() != 9) throw new IllegalArgumentException("Orgnr må være 9 tall");
+    public List<MottakerForsendelsesTyper> retrieveMottakersystemForOrgnr(String orgnr) {
+        if (orgnr == null || orgnr.length() != 9) throw new IllegalArgumentException("Orgnr må være 9 tall");
         final Request request = client.newRequest(baseUrl + "/tjenester/api/forsendelse/v1/mottakersystem/" + orgnr);
         addAuth(request);
         try {
@@ -312,7 +293,7 @@ public class SvarUtKlientApiImpl implements SvarUtKlientApi {
             else {
                 throw getHttpException(send);
             }
-        }catch (RuntimeException e) {
+        } catch (RuntimeException e) {
             throw e;
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -320,8 +301,8 @@ public class SvarUtKlientApiImpl implements SvarUtKlientApi {
     }
 
     @Override
-    public List<ForsendelsesId> retrieveForsendelsesIderByEksternRef(String eksternRef){
-        if(eksternRef == null || eksternRef.length() == 0) throw new IllegalArgumentException("Eksternref kan ikke være null eller tom");
+    public List<ForsendelsesId> retrieveForsendelsesIderByEksternRef(String eksternRef) {
+        if (eksternRef == null || eksternRef.isEmpty()) throw new IllegalArgumentException("Eksternref kan ikke være null eller tom");
         final Request request = client.newRequest(baseUrl + "/tjenester/api/forsendelse/v1/eksternref/" + eksternRef);
         addAuth(request);
         try {
@@ -332,7 +313,7 @@ public class SvarUtKlientApiImpl implements SvarUtKlientApi {
             else {
                 throw getHttpException(send);
             }
-        }catch (RuntimeException e) {
+        } catch (RuntimeException e) {
             throw e;
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -346,7 +327,7 @@ public class SvarUtKlientApiImpl implements SvarUtKlientApi {
     }
 
     private void validateForNull(ForsendelsesId forsendelseId) {
-        if(forsendelseId == null || forsendelseId.getId() == null) throw new IllegalArgumentException("ForsendelseId kan ikke være null");
+        if (forsendelseId == null || forsendelseId.getId() == null) throw new IllegalArgumentException("ForsendelseId kan ikke være null");
     }
 
     @Override
@@ -362,7 +343,7 @@ public class SvarUtKlientApiImpl implements SvarUtKlientApi {
             else {
                 throw getHttpException(send);
             }
-        }catch (RuntimeException e) {
+        } catch (RuntimeException e) {
             throw e;
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -380,24 +361,24 @@ public class SvarUtKlientApiImpl implements SvarUtKlientApi {
 
     @Override
     public void setForsendelseLestAvEksterntSystem(LestAv lestAv, ForsendelsesId forsendelseId) {
-        if(lestAv == null) throw new IllegalArgumentException("Lest av kan ikke være null");
+        if (lestAv == null) throw new IllegalArgumentException("Lest av kan ikke være null");
         validateForNull(forsendelseId);
         setForsendelseLestAvEksterntSystem(lestAv, forsendelseId.getId());
     }
+
     @Override
     public void setForsendelseLestAvEksterntSystem(LestAv lestAv, UUID forsendelseId) {
-        if(lestAv == null) throw new IllegalArgumentException("Lest av kan ikke være null");
+        if (lestAv == null) throw new IllegalArgumentException("Lest av kan ikke være null");
         validateForNull(forsendelseId);
-        final Request request = client.newRequest(baseUrl + "/tjenester/api/forsendelse/v1/" + forsendelseId + "/settLest");
-        request.method(HttpMethod.POST);
-
+        final Request request = client.newRequest(baseUrl + "/tjenester/api/forsendelse/v1/" + forsendelseId + "/settLest")
+                .method(HttpMethod.POST)
+                .body(buildJsonStringContent(lestAv));
         addAuth(request);
         try {
-            request.content(new StringContentProvider("application/json", objectMapper.writeValueAsString(lestAv), Charset.forName("UTF-8")));
             final ContentResponse send = request.send();
-            if(send.getStatus()!= 200)
+            if (send.getStatus() != 200)
                 throw getHttpException(send);
-        }catch (RuntimeException e) {
+        } catch (RuntimeException e) {
             throw e;
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -405,6 +386,53 @@ public class SvarUtKlientApiImpl implements SvarUtKlientApi {
     }
 
     private void validateForNull(UUID forsendelseId) {
-        if(forsendelseId == null) throw new IllegalArgumentException("ForsendelseId kan ikke være null");
+        if (forsendelseId == null) throw new IllegalArgumentException("ForsendelseId kan ikke være null");
     }
+
+    private MultiPartRequestContent buildMultipartContent(Forsendelse forsendelse, Map<String, InputStream> data) {
+        MultiPartRequestContent multipart = new MultiPartRequestContent();
+        multipart.addPart(buildForsendelsePart(forsendelse));
+        forsendelse.getDokumenter().stream()
+                .map(dokument -> buildFilerPart(dokument, data))
+                .forEach(multipart::addPart);
+        multipart.close();
+        return multipart;
+    }
+
+    private MultiPart.Part buildForsendelsePart(Forsendelse forsendelse) {
+        try {
+            return new MultiPart.ContentSourcePart(
+                    FORSENDELSE_PART,
+                    null,
+                    null,
+                    new StringRequestContent(
+                            "application/json",
+                            objectMapper.writeValueAsString(forsendelse)
+                    )
+            );
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Feil under serialisering av forsendelse-metadata", e);
+        }
+    }
+
+    private MultiPart.Part buildFilerPart(Dokument dokument, Map<String, InputStream> data) {
+        return new MultiPart.ContentSourcePart(
+                FILER_PART,
+                dokument.getFilnavn(),
+                null,
+                new InputStreamRequestContent(data.get(dokument.getFilnavn()))
+        );
+    }
+
+    private StringRequestContent buildJsonStringContent(Object toBeSerialized) {
+        try {
+            return new StringRequestContent(
+                    "application/json",
+                    objectMapper.writeValueAsString(toBeSerialized)
+            );
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Feil under serialisering av objekt", e);
+        }
+    }
+
 }
